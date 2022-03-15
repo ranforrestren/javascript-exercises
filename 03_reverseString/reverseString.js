6dd34d6ee9a39a604bb62ec5a9e1c94fe42fd897
Ran Ren <ranforrestren@gmail.com>
2022-03-15 19:00:30 -0400

    Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+let reversedString = "";
+for (i = 1; i < inputString.length + 1; i++)
+{
+  reversedString += inputString[inputString.length-i];
+}
+return reversedString;
 };
 
 // Do not edit below this line
