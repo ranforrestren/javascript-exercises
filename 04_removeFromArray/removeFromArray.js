36c2c00318b8cfa95c77ba433426a046b1da5b89
Ran Ren <ranforrestren@gmail.com>
2022-03-15 19:20:49 -0400

    Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(passedArray) {
+  for (let i = 0; i < passedArray.length; i++)
+  {
+    for (let it = 1; it < arguments.length; it++)
+    {
+      if (passedArray[i] === arguments[it])
+      {
+        passedArray.splice(i, 1);
+        i--;
+      }
+    }
+  }
+  return passedArray;
 };
 
 // Do not edit below this line
