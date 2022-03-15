0582aab762f598ef17abc41172d1057a3ae226df
Ran Ren <ranforrestren@gmail.com>
2022-03-15 18:52:11 -0400

    Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(inputPhrase, repeatTimes) {
+  let outputPhrase = "";
+  if (repeatTimes < 0)
+  {
+    return 'ERROR';
+  }
+  for (i = 0; i < repeatTimes; i++) 
+  {
+    outputPhrase += `${inputPhrase}`;
+  }
+  return outputPhrase;
 };
 
 // Do not edit below this line
