3a868ebf5e49735295a7b4e1e7abf40491d0365e
Ran Ren <ranforrestren@gmail.com>
2022-03-16 18:37:46 -0400

    Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+  let counter = 0;
+  if (typeof firstNumber !== 'number' || typeof secondNumber !== 'number') 
+  {
+    return 'ERROR';
+  } 
+  else if (Math.sign(firstNumber) === -1 || Math.sign(secondNumber) === -1)
+  {
+    return 'ERROR';
+  }
+  if (firstNumber > secondNumber) {
+    [firstNumber, secondNumber] = [secondNumber, firstNumber];
+  }
+  for (let i = firstNumber; i < secondNumber + 1; i++) {
+    counter += i;
+  }
+  return counter;
 };
 
 // Do not edit below this line
