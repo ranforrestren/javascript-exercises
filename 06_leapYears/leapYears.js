4a296a545e319b545ef747ae68f7edf8af525aa3
Ran Ren <ranforrestren@gmail.com>
2022-03-16 18:47:55 -0400

    Exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(inputYear) {
+if (inputYear % 4 === 0 && inputYear % 100 !== 0 || inputYear % 400 === 0)
+{
+  return true;
+} 
+else 
+{
+  return false;
+}
 };
 
 // Do not edit below this line
