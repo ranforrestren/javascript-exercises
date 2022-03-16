41a5b4816d823247c5e8d1831169109868955f58
Ran Ren <ranforrestren@gmail.com>
2022-03-16 18:53:56 -0400

    Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(inputTemp) {
+  let outputTemp = (inputTemp - 32) * 5/9;
+  outputTemp = Math.round(outputTemp * 10) / 10;
+  return outputTemp;
 };
 
-const ctof = function() {
-
+const ctof = function(inputTemp) {
+  let outputTemp = (inputTemp * 9/5) + 32;
+  outputTemp = Math.round(outputTemp * 10) / 10;
+  return outputTemp;
 };
 
 // Do not edit below this line
