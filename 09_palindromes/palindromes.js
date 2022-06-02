21c118e66bd9a029139e5a54b8d3d9308858f097
Ran Ren <ranforrestren@gmail.com>
2022-06-02 19:56:43 -0400

    Completed Palindromes Exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+  a = a.toLowerCase().replace(/[^A-Za-z0-9]/g,'');
+  let length = a.length;
+  for (let i = 0; i < length/2; i++) {
+    if (a.charAt(i) != a.charAt(length - i - 1)) {
+      return false;
+    }
+  };
+  return true;
 };
 
 // Do not edit below this line
