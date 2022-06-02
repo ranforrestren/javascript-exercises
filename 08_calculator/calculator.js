dde765d1b6f8dc1558587bd4c9408aa6564307f5
Ran Ren <ranforrestren@gmail.com>
2022-06-02 19:30:52 -0400

    Finished Calculator Exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  return a.reduce((a,b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  return a.reduce((a,b) => a * b, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let sum = 1;
+	for (let i = 1; i < a + 1; i++) {
+    sum *= i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
