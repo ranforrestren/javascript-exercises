d20ceedd542f3b88215d08300463f1ccfbf4c4d7
Ran Ren <ranforrestren@gmail.com>
2022-06-13 20:56:56 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+  if (number < 0) { return "OOPS"; }
 
+  let fib = [0, 1];
+  
+  for (i = 2; i <= number; i++) {
+    fib[i] = fib[i - 2] + fib[i - 1];
+  }
+
+  return fib[number];
 };
 
 // Do not edit below this line
