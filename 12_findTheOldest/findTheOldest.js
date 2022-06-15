676a7a650153967fb9a8c2e1576c187660a55cfc
Ran Ren <ranforrestren@gmail.com>
2022-06-14 20:05:40 -0400

    Completed Foundations 5 Exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  const oldest = people.sort((a, b) => {
+    aAge = ('yearOfDeath' in a ? a['yearOfDeath'] - a['yearOfBirth'] : 2022 - a['yearOfBirth']);
+    bAge = ('yearOfDeath' in b ? b['yearOfDeath'] - b['yearOfBirth'] : 2022 - b['yearOfBirth']);
+    return aAge > bAge ? -1 : 1;
+  })
+  return oldest[0];
 };
 
 // Do not edit below this line
